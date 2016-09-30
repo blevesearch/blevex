3a811f70de6a27db72ca4d9b15ff0c0d16385219
Marty Schoch <marty.schoch@gmail.com>
2016-09-30 16:05:50 -0400

    rename stemmer_filter to stemmer and fix travis script

--- stemmer/stemmer_filter_test.go
+++ stemmer/stemmer_filter_test.go
@@ -7,7 +7,7 @@
 //  either express or implied. See the License for the specific language governing permissions
 //  and limitations under the License.
 
-package stemmer_filter
+package stemmer
 
 import (
 	"reflect"
