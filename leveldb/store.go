blevesearch/blevex

commit bce79f677ec769471cf19ba88dd8a6e4da0ba3ef

reuse options objects within readers/writers

leveldb/store.go
@@ -60,15 +60,20 @@ func (s *Store) Close() error {
 }
 
 func (s *Store) Reader() (store.KVReader, error) {
+	snapshot := s.db.NewSnapshot()
+	options := defaultReadOptions()
+	options.SetSnapshot(snapshot)
 	return &Reader{
 		store:    s,
 		snapshot: s.db.NewSnapshot(),
+		options:  options,
 	}, nil
 }
 
 func (s *Store) Writer() (store.KVWriter, error) {
 	return &Writer{
-		store: s,
+		store:   s,
+		options: defaultWriteOptions(),
 	}, nil
 }
 

rocksdb/store.go
@@ -60,15 +60,20 @@ func (s *Store) Close() error {
 }
 
 func (s *Store) Reader() (store.KVReader, error) {
+	snapshot := s.db.NewSnapshot()
+	options := defaultReadOptions()
+	options.SetSnapshot(snapshot)
 	return &Reader{
 		store:    s,
-		snapshot: s.db.NewSnapshot(),
+		snapshot: snapshot,
+		options:  options,
 	}, nil
 }
 
 func (s *Store) Writer() (store.KVWriter, error) {
 	return &Writer{
-		store: s,
+		store:   s,
+		options: defaultWriteOptions(),
 	}, nil
 }
 

leveldb/reader.go
@@ -17,44 +17,37 @@ import (
 type Reader struct {
 	store    *Store
 	snapshot *levigo.Snapshot
+	options  *levigo.ReadOptions
 }
 
 func (r *Reader) Get(key []byte) ([]byte, error) {
-	options := defaultReadOptions()
-	options.SetSnapshot(r.snapshot)
-	b, err := r.store.db.Get(options, key)
-	options.Close()
+	b, err := r.store.db.Get(r.options, key)
 	return b, err
 }
 
 func (r *Reader) PrefixIterator(prefix []byte) store.KVIterator {
-	options := defaultReadOptions()
-	options.SetSnapshot(r.snapshot)
 	rv := Iterator{
 		store:    r.store,
-		iterator: r.store.db.NewIterator(options),
+		iterator: r.store.db.NewIterator(r.options),
 		prefix:   prefix,
 	}
-	options.Close()
 	rv.Seek(prefix)
 	return &rv
 }
 
 func (r *Reader) RangeIterator(start, end []byte) store.KVIterator {
-	options := defaultReadOptions()
-	options.SetSnapshot(r.snapshot)
 	rv := Iterator{
 		store:    r.store,
-		iterator: r.store.db.NewIterator(options),
+		iterator: r.store.db.NewIterator(r.options),
 		start:    start,
 		end:      end,
 	}
-	options.Close()
 	rv.Seek(start)
 	return &rv
 }
 
 func (r *Reader) Close() error {
+	r.options.Close()
 	r.store.db.ReleaseSnapshot(r.snapshot)
 	return nil
 }

leveldb/writer.go
@@ -17,7 +17,8 @@ import (
 )
 
 type Writer struct {
-	store *Store
+	store   *Store
+	options *levigo.WriteOptions
 }
 
 func (w *Writer) NewBatch() store.KVBatch {
@@ -60,13 +61,12 @@ func (w *Writer) ExecuteBatch(b store.KVBatch) error {
 		batch.Set(k, mergedVal)
 	}
 
-	wopts := defaultWriteOptions()
-	defer wopts.Close()
-	err := w.store.db.Write(wopts, batch.batch)
+	err := w.store.db.Write(w.options, batch.batch)
 	return err
 
 }
 
 func (w *Writer) Close() error {
+	w.options.Close()
 	return nil
 }

rocksdb/reader.go
@@ -17,13 +17,11 @@ import (
 type Reader struct {
 	store    *Store
 	snapshot *gorocksdb.Snapshot
+	options  *gorocksdb.ReadOptions
 }
 
 func (r *Reader) Get(key []byte) ([]byte, error) {
-	options := defaultReadOptions()
-	defer options.Destroy()
-	options.SetSnapshot(r.snapshot)
-	b, err := r.store.db.Get(options, key)
+	b, err := r.store.db.Get(r.options, key)
 	if err != nil {
 		return nil, err
 	}
@@ -31,33 +29,28 @@ func (r *Reader) Get(key []byte) ([]byte, error) {
 }
 
 func (r *Reader) PrefixIterator(prefix []byte) store.KVIterator {
-	options := defaultReadOptions()
-	options.SetSnapshot(r.snapshot)
 	rv := Iterator{
 		store:    r.store,
-		iterator: r.store.db.NewIterator(options),
+		iterator: r.store.db.NewIterator(r.options),
 		prefix:   prefix,
 	}
 	rv.Seek(prefix)
-	options.Destroy()
 	return &rv
 }
 
 func (r *Reader) RangeIterator(start, end []byte) store.KVIterator {
-	options := defaultReadOptions()
-	options.SetSnapshot(r.snapshot)
 	rv := Iterator{
 		store:    r.store,
-		iterator: r.store.db.NewIterator(options),
+		iterator: r.store.db.NewIterator(r.options),
 		start:    start,
 		end:      end,
 	}
 	rv.Seek(start)
-	options.Destroy()
 	return &rv
 }
 
 func (r *Reader) Close() error {
+	r.options.Destroy()
 	r.snapshot.Release()
 	return nil
 }

rocksdb/writer.go
@@ -17,7 +17,8 @@ import (
 )
 
 type Writer struct {
-	store *Store
+	store   *Store
+	options *gorocksdb.WriteOptions
 }
 
 func (w *Writer) NewBatch() store.KVBatch {
@@ -33,12 +34,11 @@ func (w *Writer) ExecuteBatch(b store.KVBatch) error {
 		return fmt.Errorf("wrong type of batch")
 	}
 
-	wopts := defaultWriteOptions()
-	err := w.store.db.Write(wopts, batch.batch)
-	wopts.Destroy()
+	err := w.store.db.Write(w.options, batch.batch)
 	return err
 }
 
 func (w *Writer) Close() error {
+	w.options.Destroy()
 	return nil
 }

commit dd4f13b393e66b0b972c8b7b312132293d50f090

fix leveldb snapshot leak
regression from bce79f67

leveldb/store.go
@@ -69,7 +69,7 @@ func (s *Store) Reader() (store.KVReader, error) {
 	options.SetSnapshot(snapshot)
 	return &Reader{
 		store:    s,
-		snapshot: s.db.NewSnapshot(),
+		snapshot: snapshot,
 		options:  options,
 	}, nil
 }